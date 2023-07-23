ec803fc8a3e2162a02a507f1d1d87683d74d4be4
jcoder58 <4xcoder@gmail.com>
2023-07-23 06:39:29 -0600

    Remove Vechile Project

--- Vehicle/Source/Vehicle/Game.Generated/SharpPlayerController.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "CoreMinimal.h"
-#include "GameFramework/PlayerController.h"
-#include "SharpPlayerController.generated.h"
-UCLASS(Blueprintable, BlueprintType, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
-class CSharpPlayerController : public APlayerController {
-GENERATED_BODY()
-public:
-};
--- Vehicle/Source/Vehicle/Game_Vehicle/CompileToCpp.bat
+++
@@ -1,4 +0,0 @@
-echo "Compiling to C++"
-cd C:\UnrealEngine\Dev\CSharpForUnrealEngine5\CSharpExamples\Compiler\bin
-.\CSharpToCpp.exe ..\..\Vehicle\Source\Vehicle\Game_Vehicle
-echo "Done"
